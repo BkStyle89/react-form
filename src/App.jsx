7eee80485ddc733c1ca2449b6fcd35eec5f6992b
BkStyle89 <kajzer@hotmail.it>
2026-03-12 15:20:52 +0100

    Milestone 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className='row'>
+            <div className='col'>
+              <ul>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
